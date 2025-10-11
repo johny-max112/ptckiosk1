87986ec0fc7bd190ac378b19d1ab83f861581628
Johntadeo Liscano <jliscano@paterostechnologicalcollege.edu.ph>
2025-10-11 09:30:17 +0800

    Add ptc_kiosk3.sql database file

---
+++ ptc_kiosk3.sql
@@ -0,0 +1,288 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 11, 2025 at 03:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ptc_kiosk3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `id` int(11) NOT NULL,
+  `mission` text DEFAULT NULL,
+  `vision` text DEFAULT NULL,
+  `history` text DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `mission`, `vision`, `history`, `last_updated`) VALUES
+(1, 'sdaasdasdsdfSDGsd', 'asdaasdaasasdfsdgfdsa', 'sdasdaasdffgdsgfhs', '2025-10-07 18:44:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `academic`
+--
+
+CREATE TABLE `academic` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `academic`
+--
+
+INSERT INTO `academic` (`id`, `title`, `content`, `created_at`) VALUES
+(1, 'asdasd', 'asdasd', '2025-10-07 17:53:17'),
+(3, 'INSTITUTE OF INFORMATION AND COMPUTING TECHNOLOGY (IICT)  ENROLLMENT', 'BSIT 2ND YEAR	8:00 AM-12:00 NN	JULY 29 & 30\nBSIT 3RD YEAR	8:00 AM-12:00 NN	JULY 31 & AUGUST 1\nBSIT 4TH YEAR	8:00 AM-12:00 NN	AUGUST 4 & 5\nCCS 2ND YEAR	8:00 AM-12:00 NN	AUGUST 6 & 7', '2025-10-08 19:10:06'),
+(4, 'Dear Students,  The Office of the Registrar would like to remind all students to strictly follow the steps below to ensure a smooth enrollment process:  Steps for Enrollment', '1.Access and print your enrollment documents:\nDownload your Certificate of Registration and Certificate of Grades.\nPrint at least two (2) copies on short bond paper.\nAffix your signature.\n\n2.Proceed to the Cashier:\nPay the required fees.\n\n3.Submit documents to the Registrar for validation.\n\n4.Validate ID at the MIS Office.\n\n5.Strictly follow your schedule.', '2025-10-08 19:12:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Johnted', 'admin2@ptc.edu.ph', '$2b$10$eaDox13FU7WNYZYDxkb0z.KKxEw7hRwzqTKhwOhLoOsWKQrmuAEhK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `announcements`
+--
+
+INSERT INTO `announcements` (`id`, `title`, `content`, `start_date`, `end_date`, `is_active`, `created_at`) VALUES
+(1, 'asd', 'asda', '2025-10-08', '2025-10-29', 1, '2025-10-07 18:34:26'),
+(2, 'asdasd', 'asdasda', '2025-10-15', '2025-10-23', 1, '2025-10-07 18:34:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campuses`
+--
+
+CREATE TABLE `campuses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `campuses`
+--
+
+INSERT INTO `campuses` (`id`, `name`, `address`) VALUES
+(1, 'PTC Main Campus', 'Pateros'),
+(2, 'PTC San Pedro Campus', 'Pateros'),
+(3, 'ITRED Campus', 'Pateros'),
+(4, 'Main Campus', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maps`
+--
+
+CREATE TABLE `maps` (
+  `id` int(11) NOT NULL,
+  `campus_id` int(11) DEFAULT NULL,
+  `campus_name` varchar(100) DEFAULT NULL,
+  `image_path` varchar(512) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `maps`
+--
+
+INSERT INTO `maps` (`id`, `campus_id`, `campus_name`, `image_path`, `description`, `address`, `created_at`) VALUES
+(6, 1, 'PTC Main Campus ', '/uploads/1759945900181.png', 'Central Main Campus', '205 College st Pateros Technological College', '2025-10-09 01:51:40'),
+(8, 2, 'PTC ITRED', '/uploads/1759947201580.png', 'Just Google \"Technical Education and Skills Development Authority Pateros\"', 'Technical Education and Skills Development Authority Pateros', '2025-10-09 02:13:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offices`
+--
+
+CREATE TABLE `offices` (
+  `id` int(11) NOT NULL,
+  `campus_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL,
+  `office_hours` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `offices`
+--
+
+INSERT INTO `offices` (`id`, `campus_id`, `name`, `description`, `contact`, `office_hours`, `created_at`) VALUES
+(1, 1, 'Registrar Office', 'Student records, enrollment, transcripts', '32543565423', 'Mon-Fri 8:00 AM - 5:00 PM', '2025-10-08 02:54:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `academic`
+--
+ALTER TABLE `academic`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `campuses`
+--
+ALTER TABLE `campuses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `maps`
+--
+ALTER TABLE `maps`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `campus_id` (`campus_id`);
+
+--
+-- Indexes for table `offices`
+--
+ALTER TABLE `offices`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `campus_id` (`campus_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `academic`
+--
+ALTER TABLE `academic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `campuses`
+--
+ALTER TABLE `campuses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `maps`
+--
+ALTER TABLE `maps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `offices`
+--
+ALTER TABLE `offices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `maps`
+--
+ALTER TABLE `maps`
+  ADD CONSTRAINT `maps_ibfk_1` FOREIGN KEY (`campus_id`) REFERENCES `campuses` (`id`);
+
+--
+-- Constraints for table `offices`
+--
+ALTER TABLE `offices`
+  ADD CONSTRAINT `offices_ibfk_1` FOREIGN KEY (`campus_id`) REFERENCES `campuses` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
